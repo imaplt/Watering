31aecdea2b1e1060a51d23d3e5cff26cb6efa3d5
imaplt <chris.auron@gmail.com>
2024-12-05 17:43:28 -0500

    Updated meta tags, timing and image handling

--- web_app.py
+++ web_app.py
@@ -283,6 +283,6 @@ def view_logs():
         )
 
 try:
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
 except Exception as e:
     print(f"Flask encountered an error: {e}")
